ebb7f20ebdfb061644a36019151ee3e5a1a7caec
MahmoudZak3 <moddzak99@yahoo.com>
2024-07-08 17:40:20 +0300

    creat edit note view

--- lib/views/widgets/notes_list_view.dart
+++ lib/views/widgets/notes_list_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'custom_note_item.dart';
+import 'note_item.dart';
 
 class NotesListView extends StatelessWidget {
   const NotesListView({super.key});
@@ -11,7 +11,7 @@ class NotesListView extends StatelessWidget {
       itemBuilder: (context, index) {
         return const Padding(
           padding: EdgeInsets.symmetric(vertical: 8),
-          child: NoteItem(),
+          child: NoteItem.NoteItem(),
         );
       },
     );
